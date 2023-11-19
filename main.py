f53a426cb77ff86b40268ef8c31d3dc2403f52e5
Alek-Kas <spawn_kas@mail.ru>
2023-11-19 22:57:13 +0300

    0.05 Программа должна запрашивать у пользователя следующую информацию: Количество паролей для генерации; Длину одного пароля; digits: 0123456789; lowercase_letters: abcdefghijklmnopqrstuvwxyz; uppercase_letters: ABCDEFGHIJKLMNOPQRSTUVWXYZ; punctuation: !#$%&*+-=?@^_. Исключать ли неоднозначные символы il1Lo0O?

--- main.py
+++ main.py
@@ -4,24 +4,53 @@ digits = "0123456789"
 lowercase_letters = "abcdefghijklmnopqrstuvwxyz"
 uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 punctuation = "!#$%&*+-=?@^_."
+ambiguous = "il1Lo0O"
 chars = ''  # будет содержать все символы, которые могут быть в генерируемом пароле.
+
+
+def password_total():
+    print('Введите количество паролей для генерации - ', end='')  # 0
+    num = input()
+    return num
+
+
+def password_len():
+    print('Введите длину одного пароля - ', end='')  # 1
+    num = input()
+    return num
+
+
+def password_digits():
+    pass
+
+
+def password_lowercase_letters():
+    pass
+
+
+def password_uppercase_letters():
+    pass
+
+
+def password_punctuation():
+    pass
+
+
+def password_ambiguous():
+    pass
 
 
 def generate_password():
     pass
 
 
-print('Количество паролей для генерации - ', end='')
-pass_total = int(input())
-print('Длину одного пароля - ', end='')
-pass_len = int(input())
-print(f'Использовать в коде {digits}', end='')
-q = input()
-print(f'Использовать в коде {lowercase_letters}', end='')
-q = input()
-print(f'Использовать в коде {uppercase_letters}', end='')
-q = input()
-print(f'Использовать в коде {punctuation}', end='')
-q = input()
+pass_total = password_total()
+pass_len = password_len()
+chars += password_digits()
+chars += password_lowercase_letters()
+chars += password_uppercase_letters()
+chars += password_punctuation()
+chars += password_ambiguous()
+
 for i in range(pass_total):
     generate_password(pass_len, chars)
