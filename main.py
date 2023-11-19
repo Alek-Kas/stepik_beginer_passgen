e6ab41bcdab335ef6e7fab77e5a7e49b6949a0a1
Alek-Kas <spawn_kas@mail.ru>
2023-11-19 22:08:15 +0300

    0.02 Программа должна запрашивать у пользователя следующую информацию: Количество паролей для генерации; Длину одного пароля;

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 punctuation = "!#$%&*+-=?@^_."
 chars = ''  # будет содержать все символы, которые могут быть в генерируемом пароле.
 
+
+def pass_gen():
+    pass
+
+
+print('Количество паролей для генерации - ', end='')
+pass_len = int(input())
+print('Длину одного пароля - ', end='')
+pass_len = int(input())
+print(f'Использовать в коде {digits}', end='')
+q = input()
+print(f'Использовать в коде {lowercase_letters}', end='')
+q = input()
+print(f'Использовать в коде {uppercase_letters}', end='')
+q = input()
+print(f'Использовать в коде {punctuation}', end='')
+q = input()
--- readme.txt
+++ readme.txt
@@ -7,12 +7,3 @@ digits: 0123456789;
 lowercase_letters: abcdefghijklmnopqrstuvwxyz;
 uppercase_letters: ABCDEFGHIJKLMNOPQRSTUVWXYZ;
 punctuation: !#$%&*+-=?@^_.
-
-Программа должна запрашивать у пользователя следующую информацию:
-Количество паролей для генерации;
-Длину одного пароля;
-Включать ли цифры 0123456789?
-Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?
-Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz?
-Включать ли символы !#$%&*+-=?@^_?
-Исключать ли неоднозначные символы il1Lo0O?
